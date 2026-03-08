6f34a1752e9f35f78d3d843be46cad92f9717230
Kacper Janiak <129707@uksw.edu.pl>
2026-03-08 10:32:21 +0100

    fix formatting

--- src/Triangle.h
+++ src/Triangle.h
@@ -11,41 +11,38 @@
 
 using namespace std;
 
-template<class T>
-class Triangle : public Shape2D<T> {
-  public:
+template <class T> class Triangle : public Shape2D<T> {
+public:
     inline virtual ShapeResult<T> compute();
 
     inline string print();
 
-    inline Triangle(const ShapeParam<T> & param);
+    inline Triangle(const ShapeParam<T>& param);
 };
 
-template<class T>
-inline ShapeResult<T> Triangle<T>::compute() {
+template <class T> inline ShapeResult<T> Triangle<T>::compute()
+{
     ShapeResult<T> result;
 
-  T base = this->m_param.get_attrib(PARAM_WIDTH);
-  T height = this->m_param.get_attrib(PARAM_HEIGHT);
+    T base = this->m_param.get_attrib(PARAM_WIDTH);
+    T height = this->m_param.get_attrib(PARAM_HEIGHT);
 
-  if (base <= 0 || height <= 0) {
-      return result;
-  }
+    if (base <= 0 || height <= 0)
+    {
+        return result;
+    }
 
-  T area = (base * height) / (T)2;
+    T area = (base * height) / (T)2;
 
-  result.set_attrib(ShapeResultIndex::RESULT_AREA, area);
+    result.set_attrib(ShapeResultIndex::RESULT_AREA, area);
 
-  return result;
+    return result;
 }
 
-template<class T>
-inline string Triangle<T>::print() {
-    return "Triangle";
-}
+template <class T> inline string Triangle<T>::print() { return "Triangle"; }
 
-template<class T>
-inline Triangle<T>::Triangle(const ShapeParam<T> & param) : Shape2D<T>(param) {
-}
+template <class T>
+inline Triangle<T>::Triangle(const ShapeParam<T>& param): Shape2D<T>(param)
+{}
 
 #endif
